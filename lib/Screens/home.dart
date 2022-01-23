743e7a528c52040ef53bba345e0bd2caa184f4e6
Akanksha1212 <singhakanksha221b@gmail.com>
2022-01-23 11:32:32 +0530

    :whale: Home Screen done

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,4 +1,6 @@
+import 'package:animated_button/animated_button.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -10,6 +12,89 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        width: size.width,
+        height: size.height,
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: glassmorphismWindow(size),
+      ),
+    );
+  }
+
+  Container glassmorphismWindow(Size size) {
+    return Container(
+      width: size.width * 0.8,
+      height: size.height * 0.9,
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.5),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedButton(
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Icon(
+                    Icons.arrow_forward_ios,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                  Text(
+                    'Start',
+                    style: GoogleFonts.odibeeSans(
+                      fontSize: 42,
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              height: 70,
+              width: 200,
+              color: Color(0xffE6C5FE),
+              onPressed: () {},
+              enabled: true,
+              shadowDegree: ShadowDegree.light,
+              duration: 400,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            AnimatedButton(
+              child: Text(
+                'Settings',
+                style: GoogleFonts.odibeeSans(
+                  fontSize: 42,
+                  fontWeight: FontWeight.w700,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.white,
+                ),
+              ),
+              height: 70,
+              width: 200,
+              color: Color(0xffC7C5FD),
+              onPressed: () {},
+              enabled: true,
+              shadowDegree: ShadowDegree.light,
+              duration: 400,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
