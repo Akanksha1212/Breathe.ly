147f9188158544fd24338082c11620c5b6d03323
Akanksha1212 <singhakanksha221b@gmail.com>
2022-01-23 10:18:23 +0530

    Splash screen done

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,84 @@
 import 'package:breathely/Screens/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // ignore: prefer_const_constructors
+    // Set landscape orientation
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
     return MaterialApp(
-      home: const Home(),
+      debugShowCheckedModeBanner: false,
+      home: Splash(),
+    );
+  }
+}
+
+class Splash extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      Duration(seconds: 3),
+      () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => Home(),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      // body: Container(
+      //   constraints: BoxConstraints.expand(),
+      //   decoration: BoxDecoration(
+      //       image: DecorationImage(
+      //           image: AssetImage("assets/images/womxn.png"),
+      //           fit: BoxFit.cover)),
+      body: Center(
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 60,
+              ),
+              Image.asset(
+                'assets/logo.png',
+                height: 250,
+                width: 250,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Image.asset(
+                'assets/logogif.gif',
+                height: 250,
+                width: 250,
+              ),
+              SizedBox(
+                height: 25,
+              ),
+            ]),
+      ),
     );
   }
 }
